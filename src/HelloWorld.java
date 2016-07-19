762fde243328d1c606cc35c74b1de673a94a20d7
saisnehad <saisnehad@gmail.com>
2016-07-19 18:49:01 -0400

    Revert "created addition of two numbers"
    This reverts commit cc7d5460eac6bba6774125f2706ba6b17778c77f.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,7 @@
-package com.Helloworld;
 public class HelloWorld
 {
 	public  static void main(String[] args) {
-		String s="@#$%^&*(";
-		System.out.println(s);
+		System.out.println("Hello world");
 	}
 }
 
