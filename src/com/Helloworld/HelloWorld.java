cc7d5460eac6bba6774125f2706ba6b17778c77f
saisnehad <saisnehad@gmail.com>
2016-07-19 18:46:08 -0400

    created addition of two numbers

--- src/com/Helloworld/HelloWorld.java
+++ src/com/Helloworld/HelloWorld.java
@@ -1,7 +1,9 @@
+package com.Helloworld;
 public class HelloWorld
 {
 	public  static void main(String[] args) {
-		System.out.println("Hello world");
+		String s="@#$%^&*(";
+		System.out.println(s);
 	}
 }
 
